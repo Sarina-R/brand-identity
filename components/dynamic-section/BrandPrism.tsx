7c41fd9360deafa50f42f3e747c650063867ce78
Sarina-R <Sarina.r.n.0@gmail.com>
2025-03-16 19:17:26 +0330

    vercel

--- components/dynamic-section/BrandPrism.tsx
+++ components/dynamic-section/BrandPrism.tsx
@@ -1,17 +1,17 @@
-import { useMDXComponents } from "@/mdx-component";
-import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+// import { useMDXComponents } from "@/mdx-component";
+// import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 
-const BrandPrism = ({ section }) => {
-  const mdxComponents = useMDXComponents();
+// const BrandPrism = ({ section }) => {
+//   const mdxComponents = useMDXComponents();
 
-  return (
-    <>
-      <MDXRemote
-        {...(section.items.MDXComponent as MDXRemoteSerializeResult)}
-        components={mdxComponents}
-      />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <MDXRemote
+//         {...(section.items.MDXComponent as MDXRemoteSerializeResult)}
+//         components={mdxComponents}
+//       />
+//     </>
+//   );
+// };
 
-export default BrandPrism;
+// export default BrandPrism;
