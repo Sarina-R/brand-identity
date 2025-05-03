a5828a9235bfd3b8c2a42695a79e7836f53e76f5
Sarina-R <Sarina.r.n.0@gmail.com>
2025-05-03 18:04:03 +0330

    BrandIdentityGlobal

--- app/api/branding/mdx.ts
+++ app/api/branding/mdx.ts
@@ -1,8 +1,8 @@
 export const BrandIdentityGlobal = `      
       <div className="">
         <div className="relative sm:flex gap-6 items-center p-4 lg:p-10 max-w-6xl mx-auto mt-12 lg:mt-20 min-h-screen">
-          <div className="flex-1 sticky top-15 h-[calc(100vh-12rem)] hidden sm:block">
-            <div className="relative top-0 w-full h-full rounded-2xl overflow-hidden ">
+          <div className="flex-1 sm:sticky sm:top-15 h-[calc(100vh-12rem)] ">
+            <div className="sm:relative sm:top-0 w-full h-full rounded-2xl overflow-hidden ">
               <Image
               width={400}
               height={400}
@@ -16,7 +16,7 @@ export const BrandIdentityGlobal = `
             <h1 className="text-xl lg:text-5xl font-extrabold mb-4 lg:mb-8 leading-tight text-[#01acec]">
               Brand Identity
             </h1>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300">
               The FIRA brand is a symbol of excellence and innovation. Our
               visual identity reflects the dynamic and futuristic nature of
               robot sports, combining modern design elements with a sense of
@@ -32,23 +32,23 @@ export const BrandIdentityGlobal = `
             <h1 className="text-xl lg:text-5xl font-extrabold mb-4 lg:mb-8 leading-tight text-[#01acec]">
               Our Mission
             </h1>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700 mb-2">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300 mb-2">
               <span className="font-semibold">Innovation:</span> Pioneering
               advancements in robotics, export constantly pushing boundaries.
             </p>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700 mb-2">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300 mb-2">
               <span className="font-semibold">Excellence:</span> Striving for
               the highest standards in competitions and education.
             </p>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700 mb-2">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300 mb-2">
               <span className="font-semibold">Unity:</span> Bringing together a
               diverse global community, celebrating collaboration.
             </p>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700 mb-2">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300 mb-2">
               <span className="font-semibold">Inspiration:</span> Motivating the
               next generation of innovators and leaders in robotics.
             </p>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300">
               <span className="font-semibold">Integrity:</span> Upholding
               fairness, transparency, and respect in all activities.
             </p>
@@ -82,7 +82,7 @@ export const BrandIdentityGlobal = `
             <h1 className="text-xl lg:text-5xl font-extrabold mb-4 lg:mb-8 leading-tight text-[#01acec]">
               Our Vision
             </h1>
-            <p className="text-sm lg:text-lg leading-6 text-neutral-700">
+            <p className="text-sm lg:text-lg leading-6 text-neutral-700 dark:text-neutral-300">
               At FIRA, we envision a world where robot sports inspire
               creativity, collaboration, and technological advancement. Our
               mission is to provide a platform for enthusiasts, engineers, and
